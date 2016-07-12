976d9193da49afe3a6f80f3a49d24e6741ca2c93
Erick Almeida <ephillipe@gmail.com>
2016-07-12 13:15:28 -0300

    Update nfs-client.sh

--- nfs-client.sh
+++ nfs-client.sh
@@ -11,10 +11,9 @@ for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
   target=$(echo $mnt | awk -F':' '{ print $2 }')
   targets+=("$target")
-
   mkdir -p $target
-
   mount -t nfs -o proto=tcp,port=2049 ${NFS_PORT_2049_TCP_ADDR}:${src} ${target}
+  echo "mount -t nfs -o proto=tcp,port=2049 ${NFS_PORT_2049_TCP_ADDR}:${src} ${target}"
 done
 
 exec inotifywait -m "${targets[@]}"
